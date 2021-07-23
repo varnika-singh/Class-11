952d5c9795c387afdbc088082ebbf6f6f99ae633
varnika-singh <sing.varnika@gmail.com>
2021-07-23 23:13:01 +0530

    Added numberutils package

--- main.py
+++ main.py
@@ -2,4 +2,15 @@ from numberutils import analyzer
 
 if __name__ == '__main__':
     nu = analyzer.NumberAnalyzer()
-    print("{} is even: {}".format(5, nu.is_even(5)))
+    print("\nhi!! this program finds a number is even or not. 0 to exit\n")
+    while True:
+        try:
+            num = int(input("Number: "))
+            if num == 0:
+                break
+            if nu.is_even(num) == True:
+                print("{} is even".format(num))
+            else:
+                print("{} is odd".format(num))
+        except Exception as e:
+            print("enter a valid number. try again...")
